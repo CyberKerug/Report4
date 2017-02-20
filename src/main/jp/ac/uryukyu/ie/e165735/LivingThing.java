f29ce634553ac8b825e7491a0b99b0f522fa4180
Takeaki Onaga <e165735@ie.u-ryukyu.ac.jp>
2017-02-20 20:48:29 +0900

    New Class "LivingThing"

---
+++ src/main/jp/ac/uryukyu/ie/e165735/LivingThing.java
@@ -0,0 +1,46 @@
+package jp.ac.uryukyu.ie.e165735;
+
+import java.security.PrivateKey;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public LivingThing (String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attack(LivingThing opponent){
+        if(dead == false) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint <= 0 ) {
+            dead = true;
+            if (name == "Hero") {
+                System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+            } else {
+                System.out.printf("モンスター%sは倒れた。\n", name);
+            }
+        }
+    }
+}
