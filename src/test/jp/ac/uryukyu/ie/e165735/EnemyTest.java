6583aeb44d931128e4a5e84e9b35072da4d3a9d5
Takeaki Onaga <e165735@ie.u-ryukyu.ac.jp>
2017-02-22 12:18:21 +0900

    Test code written

--- src/test/jp/ac/uryukyu/ie/e165735/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165735/EnemyTest.java
@@ -7,9 +7,14 @@ import static org.junit.Assert.*;
 //敵が死んでも攻撃できる点についての修正(幽霊は物理的に殴れない)
 public class EnemyTest {
     @Test
+    public void isDead() throws Exception {
+        assertTrue(true);
+    }
+    @Test
     public void attack() throws Exception {
 
         assertEquals(0, 0);
+
     }
 
 }
